0cde3c703159bc05fd863ff340e006b426d3de0a
Nicole Bruneau <nicole_bruneau@hotmail.com>
2021-09-22 17:45:44 -0500

    changes en el java

--- IntArray.java
+++ IntArray.java
@@ -1,4 +1,9 @@
 public class IntArray {
     public int[] arr;
     public int length;
+ 
+    public IntArray(int length){
+        arr = new int[length];
+        this.length = length;
+    }
 }
--- Problema1.java
+++ Problema1.java
@@ -1,30 +1,30 @@
 public class Problema1 {
     public static void main(String[] args) {
-        IntArray myarr = new IntArray();
+        IntArray myarr = new IntArray(100);
         initIntArray(myarr);
         printArray(myarr);
-
+ 
     }
-
+ 
     IntArray createIntArray(int size) {
         // Esto no es necesario pues es manejado por el new
         return null;
     }
-
+ 
     static void initIntArray(IntArray that) {
-
+ 
         for (int i = 0; i < that.length; i++) {
             that.arr[i] = i;
         }
-
+ 
     }
-
+ 
     static void printArray(IntArray that) {
         for (int i = 0; i < that.length; i++) {
             System.out.println("that.arr[" + i + "] = " + that.arr[i]);
         }
     }
-
+ 
     void destroyIntArray(IntArray that) {
         // Java posee un garbage collector que realiza esta tarea
         // automáticamente
