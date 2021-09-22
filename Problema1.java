dd45829e76cf0dfefdf7a3201bcb5b010e56396a
Nicole Bruneau <nicole_bruneau@hotmail.com>
2021-09-22 17:10:17 -0500

    modificaciones en problema1.java

--- Problema1.java
+++ Problema1.java
@@ -2,4 +2,22 @@ public class Problema1 {
     public static void main(String[] args) {
         IntArray myarr = new IntArray();
     }
+
+    IntArray createIntArray(int size){
+        return 0;
+    }
+    void initIntArray(IntArray this){
+
+    }
+    void printArray(IntArray this){
+         for (int i = 0; i < this.length; i++){
+             System.out.printf();
+         }
+    }
+    void destroyIntArray(IntArray this){ 
+        //Java posee un garbage collector que realiza esta tarea
+        //automáticamente
+    }
 }
+
+
