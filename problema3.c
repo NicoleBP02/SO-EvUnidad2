50ce25a6dcb913bc32a7f6f9f4ed3a15f455709a
Miguel <miguel.perezf@upb.edu.co>
2021-09-24 22:59:33 -0500

    Funciones problema 3

--- problema3.c
+++ problema3.c
@@ -1,26 +1,70 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <ctype.h>
+#include <string.h>
+#include <pthread.h>
 
-int main(int argc, char **argv){
+void upper(char *buffer);
+void contarPalabras(char *buffer);
 
-    FILE *fin = fopen(argv[1],"r");
-    if (fin == NULL){
+int main(int argc, char **argv)
+{
+    FILE *fin = fopen(argv[1], "r");
+    if (fin == NULL)
+    {
         perror("Imagen 2 Falla en la apertura del archivo de entrada: ");
-        return(EXIT_FAILURE);
+        return (EXIT_FAILURE);
     }
 
-    char buffer[128];
-    char *status =  NULL;
-    do{
-        status = fgets(buffer, sizeof(buffer),fin);
-        if(status != NULL){
-            printf("%s",buffer);
+    char buffer[256];
+    char *status = NULL;
+    do
+    {
+        status = fgets(buffer, sizeof(buffer), fin);
+        if (status != NULL)
+        {
+            printf("Original: %s", buffer);
             sleep(1);
         }
-    }while (status !=NULL);
-    printf("\n");
+    } while (status != NULL);
+    //printf("\n");
+}
 
+void upper(char *buffer)
+{
+    for (int i = 0; buffer[i] != '\0'; ++i)
+    {
+        buffer[i] = toupper(buffer[i]);
+    }
+    printf("Procesada (UPPER): %s\n", buffer);
+}
+
+void contarPalabras(char *buffer)
+{
+    int contador = 0;
+    int largo = strlen(buffer);
+
+    for (int i = 0; i < largo; i++)
+    {
+        if (buffer[i] == ' ' && buffer[i + 1] != ' ')
+        {
+            contador++;
+        }
+    }
+    printf("Procesada (cantidad de palabras): %d \n", contador);
+}
+
+void contarVocales(char *cadena)
+{
+	int vocales = 0;
+	for (int indice = 0; cadena[indice] != '\0'; ++indice)
+    {
+		char letraActual = tolower(cadena[indice]);
+		if (letraActual == 'a' || letraActual == 'e' || letraActual == 'i' || letraActual == 'o' || letraActual == 'u')
+		{
+			vocales++;
+		}
+	}
+	printf("Procesada (cantidad de vocales): %d \n", vocales);
 }
