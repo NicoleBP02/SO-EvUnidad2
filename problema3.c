4137d4e0e67d1d0cbcb99beb7edad7f6061f6713
Miguel <miguel.perezf@upb.edu.co>
2021-09-25 21:29:27 -0500

    Correccion detalle

--- problema3.c
+++ problema3.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
     FILE *fin = fopen(argv[1], "r");
     if (fin == NULL)
     {
-        perror("Imagen 2 Falla en la apertura del archivo de entrada: ");
+        perror("Falla en la apertura del archivo de entrada: ");
         return (EXIT_FAILURE);
     }
 
